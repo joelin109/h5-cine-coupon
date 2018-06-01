2cec98417c9081ce572572fda9d20154ea1b0da9
joelin109 <joe.lin1009@gmail.com>
2018-06-01 18:30:53 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    cache: true,
+    devtool: "eval",
+    entry: {
+        "index": './src/webapp/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'www'),
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].[chunkhash:4].js'
+
+    },
+    externals: {
+        //'react': 'React',
+        //'react-dom': 'ReactDOM'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.resolve(__dirname, 'src'),
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react', "stage-0"],
+                    plugins: ["transform-decorators-legacy"]
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!sass-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src/webapp'),
+        }
+    },
+    stats: {
+        colors: true
+    },
+    //devtool: 'eval',
+    devtool: 'cheap-module-source-map',
+
+    plugins: [
+        new webpack.DllReferencePlugin({
+            context: __dirname,
+            manifest: require('./www/dll/manifest-vendor-dll.json')
+        })
+    ]
+};
